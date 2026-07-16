27fccb4cdab43063e256cfa62aff7c7ec72322e3
Nick <jnb5.business@gmail.com>
2026-07-15 22:16:17 -0400

    test

--- solutions/math/0067-add-binary.py
+++ solutions/math/0067-add-binary.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<< << << < HEAD
 a = "11"
 b = "1"
 solution = ""
@@ -22,7 +22,7 @@ elif (a[::-1] != b[::-1]) and carry == "1":
     solution += "0"
     print(f"solution: {solution}")
 else:
-    solution += "1" 
+    solution += "1"
     print(f"solution: {solution}")
 
 if carry == "1":
@@ -30,10 +30,11 @@ if carry == "1":
 
 solution = solution[::-1]
 print(solution)
-=======
+== == == =
 # 1st solution: time complexity: O(N), beats: 52.88%
 # this took way longer than i want to admit lol. ended up looping through based on the length of the strings and
 # abused if statments for each case using a carry variable to handle the binary addition logic.
+
 
 class Solution(object):
     def addBinary(self, a, b):
@@ -80,9 +81,6 @@ class Solution(object):
 # a better solution which (imo) is not in the spirit of the question but it works
 
         return bin(int(a, 2) + int(b, 2))[2:]
-<<<<<<< HEAD
 
-    # update for git
-=======
->>>>>>> 35d673538edc858b4769c332522acbb90beded70
->>>>>>> 51dd203827b186c4e4935c22dce4c0ccf3e8117d
+
+# update for git
