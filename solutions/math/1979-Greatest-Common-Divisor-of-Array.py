2aaf2f0116f7d53bd4651d2a1673bf4ea9fa2b76
Nick <jnb5.business@gmail.com>
2026-07-17 22:51:58 -0400

    problem 1979: greatest common divisor of an array

---
+++ solutions/math/1979-Greatest-Common-Divisor-of-Array.py
@@ -0,0 +1,32 @@
+# first try Beats %11.08
+
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums.sort()
+        smallest = nums[0]
+        largest = nums[len(nums) - 1]
+        denom = 1
+        result = 1
+
+        while denom <= smallest:
+            if smallest % denom == 0 and largest % denom == 0:
+                result = denom
+            denom += 1
+
+        return result
+
+    # second try uses fancy math
+
+    class Solution(object):
+
+    def findGCD(self, nums):
+        large = max(nums)
+        small = min(nums)
+        while small != 0:
+            large, small = small, large % small
+        return large
