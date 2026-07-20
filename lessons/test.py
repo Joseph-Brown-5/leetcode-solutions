6731b25a4a7daec82bbd38dfe1d6cd37c94be1e7
Nick <jnb5.business@gmail.com>
2026-07-20 16:02:08 -0400

    started work on the test problem zig zag thing

--- lessons/test.py
+++ lessons/test.py
@@ -1,19 +1,51 @@
+def zigzagger(s, numRows):
+    # zig
+    flipper = 0
+    row = 0
+    stepper = 0
+    middleCount = 0
+    zigVar = 1
+    zagStep = 0
+    container = [[] for _ in range(numRows)]
+    
+    for x, character in enumerate(s):
+        if flipper == 0:
+            if stepper <= numRows:
+                container[row].append(character)
+                stepper += 1
+                row += 1
+            if stepper == numRows:
+                flipper = 1
+                stepper = 0
+                row = 0
+        elif flipper == 1:
+            while middleCount <= (numRows):
+                if middleCount == (numRows - zigVar):
+                    container[numRows - zigVar].append(character)
+                    middleCount += 1
+                    zigVar += 1
+                else:
+                    container[middleCount-1].append(" ")
+                    middleCount += 1
+                zagStep += 1
+                if zagStep == (numRows - 2):
+                    flipper = 0
+                    zigVar = 1
+                    zagStep = 0
+                    middleCount = 0
+        
+
+    for row in container:
+        print("".join(row))
+
+    # zag
+
+    return
+    
+
 
 s = "paypalishiring"
 
-test = [["P","","A","","H","","N"],
-        ["A","P","L","S","I","I","G"],
-        ["Y","","I","","R"]]
+r = int(input("how many rows: "))
 
-print(test)
-
-print(test[0][2])
-
-def convert(s, numRows):
-    flipper = 0
-
-    # zig
-    
-    # zag
-    
-    return
+zigzagger(s,r)
